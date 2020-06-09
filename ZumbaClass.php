74db2a54c328b5a5408821451dee31cf28317403
Wisdom-22 <66562629+Wisdom-22@users.noreply.github.com>
2020-06-09 15:58:52 +0100

    This is the zumba class page

---
+++ ZumbaClass.php
@@ -0,0 +1,88 @@
+
+<?php 
+ include('header.php');
+?>
+<section class = "trainer"  style= "padding:50px; text-align:center;">
+    <h3>Welcome</h3>
+		<div class="container">
+			<div class="row">
+				<div class="col">
+				   <iframe width ="300" height ="200" src ="https://www.youtube.com/embed/bm4WZyH5p2I" frameborder="0" allowfullscreen></iframe>
+				</div>
+				<div class="col-6">
+				   <h5>Introduction to Zumba</h5>
+				   <small>
+                   Start here with beginner yoga sequences, tutorials on foundational yoga poses, and answers to your questions on all the yoga basics. Build strength and confidence to take your yoga practice deeper.
+                   Even if you have chronic fatigue, fibromyalgia, or autoimmune diseases like Lyme or rheumatoid arthritis and have to spend part (or most) of the day.
+				   </small>
+				</div>
+				<div class="col">
+				<button type="button" class="btn btn-secondary btn-lg">Watch Now</button>
+				</div>
+			</div>
+		</div>
+</section>
+
+<section class = "trainer"  style= "padding:50px; text-align:center;">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+				   <iframe width ="300" height ="200" src ="https://www.youtube.com/embed/-VXhoeaxxi0" frameborder="0" allowfullscreen></iframe>
+				</div>
+				<div class="col-6">
+				   <h5>Advanced Zumba</h5>
+				   <small>
+                   It is a complete list of most advanced zumba poses (asanas) along with some intermediate positions modified to level up your home yoga practice for improved strength, mental focus, flexibility and better health and body. It’s time to get going and challenge everyone by performing the hardest of hard zumba poses with a master zumba ease.
+				   </small>
+				</div>
+				<div class="col">
+				<button type="button" class="btn btn-secondary btn-lg">Watch Now</button>
+				</div>
+			</div>
+		</div>
+</section>
+
+<section class = "trainer"  style= "padding:50px; text-align:center;">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+				   <iframe width ="300" height ="200" src ="https://www.youtube.com/embed/GV2qtIOrMgs" frameborder="0" allowfullscreen></iframe>
+				</div>
+				<div class="col-6">
+				   <h5>Lose Belly fat with zumba dance</h5>
+				   <small>
+                   Advanced Zumba trainings for Flexibility Zumba is an ancient discipline that originated in the country of India. The practice involves the mental, physical and spiritual discipline. It combines different Asanas or poses, breathing techniques or Pranayama, and meditation.
+				   </small>
+				</div>
+				<div class="col">
+				<button type="button" class="btn btn-secondary btn-lg">Watch Now</button>
+				</div>
+			</div>
+		</div>
+</section>
+
+<section class = "trainer"  style= "padding:50px; text-align:center;">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+				   <iframe width ="300" height ="200" src ="https://www.youtube.com/embed/HRkNfdlm5Qs" frameborder="0" allowfullscreen></iframe>
+				</div>
+				<div class="col-6">
+				   <h5>Zumba for weight Loss</h5>
+				   <small>
+                   Yin Zumba Sequence: 60 Minutes (Challenge in Stillness) - Reconnected.me This sequence came about after a challenge I was presented with at a studio I have recently been covering Yin classes at. It includes some of the more challenging, yang-like poses that I wouldn’t normally teach in class.But its yours free here
+				   </small>
+				</div>
+				<div class="col">
+				<button type="button" class="btn btn-secondary btn-lg">Watch Now</button>
+				</div>
+			</div>
+		</div>
+</section>
+
+
+
+
+<?php 
+ include('footer.php');
+?>
