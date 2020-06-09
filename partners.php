5fbdd3c96165cb406e4dd932e79c69861db18e2c
Wisdom-22 <66562629+Wisdom-22@users.noreply.github.com>
2020-06-09 15:30:52 +0100

    This is the partners page

---
+++ partners.php
@@ -0,0 +1,70 @@
+<!-- written by muiz johnson-->
+<?php include('header.php'); ?>
+
+<h3><em class ="lead">For several years have been able to partner with these companies.</em></h3>
+<hr class="mb-4">
+<section>
+
+<div class="container">
+  <div class="row">
+        <div class="col">
+        <img src ="images/add.jpg" alt ="" style = "max-width:100px;" />
+        </div>
+        <div class="col">
+        <img src ="images/chev.jpg" alt ="" style = "max-width:100px;" />
+        </div>
+        <div class="col">
+        <img src ="images/fila.png" alt ="" style = "max-width:100px;" />
+        </div>
+        <div class="col">
+        <img src ="images/ever.png" alt ="" style = "max-width:100px;" />
+        </div>
+  </div>
+</div>
+<hr class="mb-4">
+<div class="container">
+  <div class="row">
+        <div class="col">
+        <img src ="images/jd.png" alt ="" style = "max-width:100px;" />
+        </div>
+        <div class="col">
+        <img src ="images/gold.png" alt ="" style = "max-width:100px;" />
+        </div>
+        <div class="col">
+        <img src ="images/sup.png" alt ="" style = "max-width:100px;" />
+        </div>
+        <div class="col">
+        <img src ="images/mount.png" alt ="" style = "max-width:100px;" />
+        </div>
+  </div>
+</div>
+<hr class="mb-4">
+<div class="container">
+  <div class="row">
+        <div class="col">
+        <img src ="images/etihad.png" alt ="" style = "max-width:100px;" />
+        </div>
+        <div class="col">
+        <img src ="images/red.png" alt ="" style = "max-width:100px;" />
+        </div>
+        <div class="col">
+        <img src ="images/gympole.jpg" alt ="" style = "max-width:100px;" />
+        </div>
+        <div class="col">
+        <img src ="images/gymfind.jpg" alt ="" style = "max-width:100px;" />
+        </div>
+  </div>
+</div>
+
+
+
+</section>
+
+
+
+
+
+
+<?php include('footer.php'); ?>
+
+
