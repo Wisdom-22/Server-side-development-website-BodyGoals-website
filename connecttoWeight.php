98ae09cef88950d38d55ccdcab0a987564a3b071
Chukwuemeka Wisdom Arinze <chukwuemekawisdom.arinze@student.griffith.ie>
2020-06-09 15:12:53 +0100

    This is validation connection for connectToWeight

---
+++ connecttoWeight.php
@@ -0,0 +1,20 @@
+<?php
+	session_start();
+
+    if(isset($_SESSION['username'])){
+		//start a time for the user
+        $time = time() + 86400;
+        require('WeightLifting.php');
+        
+    }
+    else{
+        require('header.php');
+		echo "<section style='padding:100px;'>
+				<h2>Sorry you need to login or register!!!.</h2>
+				<i class='fas fa-times-circle fa-8x'></i>
+				<p>We would get back to you shortly...</p>
+				<a href = 'index.php'>Go to Home Page</a>
+			</section>";
+		require('footer.php');
+    }
+?>
