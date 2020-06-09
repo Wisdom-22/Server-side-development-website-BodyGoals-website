adbad8ba721a9915a7e09453eb5f814b1bafb939
Wisdom-22 <66562629+Wisdom-22@users.noreply.github.com>
2020-06-09 15:24:25 +0100

    This is the footer page

---
+++ footer.php
@@ -0,0 +1,61 @@
+
+<!DOCTYPE html>
+<html lang = "en">
+	<head>
+		<!-- head section --> 
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<title>BodyGoals</title>
+		<link rel ="icon" href ="images/icon">
+		<link rel ="stylesheet" href="bodygoals.css">
+		<link rel="canonical" href="https://getbootstrap.com/docs/4.4/examples/carousel/">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+		<!-- Bootstrap core CSS -->
+		<link href="/docs/4.4/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+		<!--boostrap javaScript -->
+		<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+		<script src="https://kit.fontawesome.com/28ced351d4.js" crossorigin="anonymous"></script>
+		
+			<!-- Favicons -->
+		<link rel="apple-touch-icon" href="/docs/4.4/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
+		<link rel="icon" href="/docs/4.4/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
+		<link rel="icon" href="/docs/4.4/assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
+		<link rel="manifest" href="/docs/4.4/assets/img/favicons/manifest.json">
+		<link rel="mask-icon" href="/docs/4.4/assets/img/favicons/safari-pinned-tab.svg" color="#563d7c">
+		<link rel="icon" href="/docs/4.4/assets/img/favicons/favicon.ico">
+		<meta name="msapplication-config" content="/docs/4.4/assets/img/favicons/browserconfig.xml">
+		<meta name="theme-color" content="#563d7c">
+	</head>
+	<body>
+			<footer class = "footer">
+			<a href = "index.php"> 
+				<i class="icon fas fa-dumbbell fa-5x"></i>
+			</a>
+			
+			<div>
+				<a class="navbar-brand" href="index.php" style="color:black; float:left;"> <strong>BODYGOALS</strong></a>
+			</div>
+			
+			<p><strong>© Copyright 2020 BODYGOALS</strong></p> 
+			
+			<div class = "footer-div" style="margin-left:300px;"> 
+					<ul class="list-inline">
+						<li class="list-inline-item"><a href="contactus.php"><strong>Contact Us</strong></a></li>
+						<li class="list-inline-item"><a href="aboutUs.php"><strong>About Us</strong></a></li>
+						<li class="list-inline-item"><a href="partners.php"><strong>Partnerships</strong></a></li>
+						<li class="list-inline-item"><a href="developers.php"><strong>Developers</strong></a></li>
+					</ul>
+			</div>
+		
+			<div style = "margin-bottom:10rem; margin:0 auto;">
+					<i class="fab fa-twitter" style="margin: 20px 10px;"></i>
+					<i class="fab fa-facebook-f" style="margin: 20px 10px;"></i>
+					<i class="fab fa-instagram" style="margin: 20px 10px;"></i>
+					<i class="fas fa-envelope" style="margin: 20px 10px;"></i>
+			</div>
+		</footer>
+	</body>
+</html>
