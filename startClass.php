1e28a496c9ec1924cafcecd963f7417e8a9102e8
Wisdom-22 <66562629+Wisdom-22@users.noreply.github.com>
2020-06-09 15:40:23 +0100

    This is the start class page

---
+++ startClass.php
@@ -0,0 +1,91 @@
+<?php
+    session_start();
+    $username = $_SESSION['username'];
+?>
+<?php 
+ include('header.php');
+?>
+<section class = "trainer"  style= "padding:50px; text-align:center;">
+    <h3>Welcome</h3>
+		<div class="container">
+			<div class="row">
+				<div class="col">
+				   <iframe width ="300" height ="200" src ="https://www.youtube.com/embed/VaoV1PrYft4" frameborder="0" allowfullscreen></iframe>
+				</div>
+				<div class="col-6">
+				   <h5>Introduction to Yoga</h5>
+				   <small>
+                   Start here with beginner yoga sequences, tutorials on foundational yoga poses, and answers to your questions on all the yoga basics. Build strength and confidence to take your yoga practice deeper.
+                   Even if you have chronic fatigue, fibromyalgia, or autoimmune diseases like Lyme or rheumatoid arthritis and have to spend part (or most) of the day.
+				   </small>
+				</div>
+				<div class="col">
+				<button type="button" class="btn btn-secondary btn-lg">Watch Now</button>
+				</div>
+			</div>
+		</div>
+</section>
+
+<section class = "trainer"  style= "padding:50px; text-align:center;">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+				   <iframe width ="300" height ="200" src ="https://www.youtube.com/embed/t2z8ZsAP2Vw" frameborder="0" allowfullscreen></iframe>
+				</div>
+				<div class="col-6">
+				   <h5>Advanced Yoga poses</h5>
+				   <small>
+                   It is a complete list of most advanced yoga poses (asanas) along with some intermediate positions modified to level up your home yoga practice for improved strength, mental focus, flexibility and better health and body. It’s time to get going and challenge everyone by performing the hardest of hard yoga poses with a master yogi’s ease.
+				   </small>
+				</div>
+				<div class="col">
+				<button type="button" class="btn btn-secondary btn-lg">Watch Now</button>
+				</div>
+			</div>
+		</div>
+</section>
+
+<section class = "trainer"  style= "padding:50px; text-align:center;">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+				   <iframe width ="300" height ="200" src ="https://www.youtube.com/embed/Yzm3fA2HhkQ" frameborder="0" allowfullscreen></iframe>
+				</div>
+				<div class="col-6">
+				   <h5>Flexiblity and Strength</h5>
+				   <small>
+                   Advanced Yoga Poses for Flexibility Yoga is an ancient discipline that originated in the country of India. The practice involves the mental, physical and spiritual discipline. It combines different Asanas or poses, breathing techniques or Pranayama, and meditation.
+				   </small>
+				</div>
+				<div class="col">
+				<button type="button" class="btn btn-secondary btn-lg">Watch Now</button>
+				</div>
+			</div>
+		</div>
+</section>
+
+<section class = "trainer"  style= "padding:50px; text-align:center;">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+				   <iframe width ="300" height ="200" src ="https://www.youtube.com/embed/v6oE5zNbt1c" frameborder="0" allowfullscreen></iframe>
+				</div>
+				<div class="col-6">
+				   <h5>60-Minutes advanced Yoga Flow</h5>
+				   <small>
+                   Yin Yoga Sequence: 60 Minutes (Challenge in Stillness) - Reconnected.me This sequence came about after a challenge I was presented with at a studio I have recently been covering Yin classes at. It includes some of the more challenging, yang-like poses that I wouldn’t normally teach in class.But its yours free here
+				   </small>
+				</div>
+				<div class="col">
+				<button type="button" class="btn btn-secondary btn-lg">Watch Now</button>
+				</div>
+			</div>
+		</div>
+</section>
+
+
+
+
+<?php 
+ include('footer.php');
+?>
