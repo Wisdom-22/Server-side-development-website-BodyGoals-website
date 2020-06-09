2522ca9b4565fd8fbd01274330890ffde9a4fa23
Wisdom-22 <66562629+Wisdom-22@users.noreply.github.com>
2020-06-09 15:31:53 +0100

    This is the personal trainer page

---
+++ personal_trainer.php
@@ -0,0 +1,94 @@
+<!-- written by Chukwuemeka Wisdom Arinze -->
+<?php 
+ include('header.php');
+?>
+
+<section class = "trainer"  style= "padding:50px; text-align:center;">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+				   <img style = "max-width:20rem;" src = "images/trainer1.png" alt = ""/>
+				</div>
+				<div class="col-6">
+				   <h5>Jason Bourne</h5>
+				   <small>
+						Jason Bourne was a former U.S Marine who served his country for 10 years until he was honourably discharged. After he was dischaged, he decided to go to university and study physical fitness. He graduated with flying colors and immediately started working with us. He has worked with us for 2 years and knows exactly how to get you in shape. With experiences that he has in the Marine corp and from his time in the university, he has what it takes to get you your result.
+				   </small>
+				</div>
+				<div class="col">
+				<i class="fas fa-search-location"></i><p></p>
+				<a href = "contactus.php"><button type="button" class="btn btn-secondary btn-lg">Contact</button></a>
+				</div>
+			</div>
+		</div>
+</section>
+
+<section>
+		<div class="container">
+			<div class="row">
+				<div class="col">
+				   <img style = "max-width:20rem;" src = "images/trainer2.jpg" alt = ""/>
+				</div>
+				<div class="col-6">
+				   <h5>David Webb</h5>
+				   <small>
+						David Webb was a Captain in the Army, He served for 12 years and was honourably discharged with. He spent have of his service training other soldiers, so that they can have the physical abilities they need. After the Army, he decided to go to the university to study to be a personal trainer. He has the tools needed to help any body get their body goals and also help with the dieting of his clients. 
+				   </small>
+				</div>
+				<div class="col">
+				<i class="fas fa-search-location"></i><p></p>
+				<a href = "contactus.php"><button type="button" class="btn btn-secondary btn-lg">Contact</button></a>
+				</div>
+			</div>
+		</div>
+</section>
+
+<section>
+
+<div class="container">
+			<div class="row">
+				<div class="col">
+				   <img style = "max-width:20rem;" src = "images/trainer3.jpg" alt = ""/>
+				</div>
+				<div class="col-6">
+				   <h5>Catherine Ambrose</h5>
+				   <small>
+						Catherine Ambrose was with the british S.A.S and spent just 8 years with them before taken on a different career path. Before that, She was former Miss World and beauty peagent winner. She studied Physical Fitness at Cambridge Univerisy and graduated with flying colors. She has trained so many individuals and also has a method she has developed that has a result percantage of 95%. 
+				   </small>
+				</div>
+				<div class="col">
+				<i class="fas fa-search-location"></i><p></p>
+				<a href = "contactus.php"><button type="button" class="btn btn-secondary btn-lg">Contact</button></a>
+				</div>
+			</div>
+		</div>
+</section>
+
+<section>
+
+<div class="container">
+			<div class="row">
+				<div class="col">
+				   <img style = "max-width:20rem;" src = "images/trainer4.jpg" alt = ""/>
+				</div>
+				<div class="col-6">
+				   <h5>Maggi Shawn</h5>
+				   <small>
+						Maggi Shawn is a certified personal trainer that got he degree from Havard Univeristy. She studied body anatomy and nutrition and graduated top of her class. She is has worked with so many athletes, key political figures, celebritiesand has been recommended by hospitals because of her vast knowledge about nutrition and diet. She has the capabilities to ensure that you get the best out of any of your body types.
+				   </small>
+				</div>
+				<div class="col">
+				<i class="fas fa-search-location"></i><p></p>
+				<a href = "contactus.php"><button type="button" class="btn btn-secondary btn-lg">Contact</button></a>
+				</div>
+			</div>
+		</div>
+</section>
+
+
+
+
+
+<?php 
+ include('footer.php');
+?>
