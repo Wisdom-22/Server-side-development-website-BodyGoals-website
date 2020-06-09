ef16c27f5823566c7f90931e83913d6b24e5b380
Chukwuemeka Wisdom Arinze <chukwuemekawisdom.arinze@student.griffith.ie>
2020-06-09 15:03:28 +0100

    This is cardio page

---
+++ Cardio.php
@@ -0,0 +1,87 @@
+<?php 
+ include('header.php');
+?>
+<section class = "trainer"  style= "padding:50px; text-align:center;">
+    <h3>Welcome</h3>
+		<div class="container">
+			<div class="row">
+				<div class="col">
+				   <iframe width ="300" height ="200" src ="https://www.youtube.com/embed/sjmsApWcyCU" frameborder="0" allowfullscreen></iframe>
+				</div>
+				<div class="col-6">
+				   <h5>10 Minutes Home cardio</h5>
+				   <small>
+                   Start here with beginner cardio sequences, tutorials on foundational cardio poses, and answers to your questions on all the cardio basics. Build strength and confidence to take your endurance deeper.
+                   Even if you have chronic fatigue, fibromyalgia, or autoimmune diseases like Lyme or rheumatoid arthritis and have to spend part (or most) of the day.
+				   </small>
+				</div>
+				<div class="col">
+				<button type="button" class="btn btn-secondary btn-lg">Watch Now</button>
+				</div>
+			</div>
+		</div>
+</section>
+
+<section class = "trainer"  style= "padding:50px; text-align:center;">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+				   <iframe width ="300" height ="200" src ="https://www.youtube.com/embed/eWjd8C2SB0Y" frameborder="0" allowfullscreen></iframe>
+				</div>
+				<div class="col-6">
+				   <h5>Intense Cardio Workouts</h5>
+				   <small>
+                   It is a complete list of most intense and advanced cardio Workouts along with some intermediate positions modified to level up your home cario practice for improved strength, mental focus, flexibility and better health and body. It’s time to get going and challenge everyone by performing the hardest of hard cardio workouts with a master cardio ease.
+				   </small>
+				</div>
+				<div class="col">
+				<button type="button" class="btn btn-secondary btn-lg">Watch Now</button>
+				</div>
+			</div>
+		</div>
+</section>
+
+<section class = "trainer"  style= "padding:50px; text-align:center;">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+				   <iframe width ="300" height ="200" src ="https://www.youtube.com/embed/2Wdsm_bI88Y" frameborder="0" allowfullscreen></iframe>
+				</div>
+				<div class="col-6">
+				   <h5>Bodyweight core workout</h5>
+				   <small>
+                   Advanced bodyweight workouts for core flexibility is an ancient discipline that originated in the country of India. The practice involves the mental, physical and spiritual discipline. It combines different Asanas or poses, breathing techniques or Pranayama, and meditation.
+				   </small>
+				</div>
+				<div class="col">
+				<button type="button" class="btn btn-secondary btn-lg">Watch Now</button>
+				</div>
+			</div>
+		</div>
+</section>
+
+<section class = "trainer"  style= "padding:50px; text-align:center;">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+				   <iframe width ="300" height ="200" src ="https://www.youtube.com/embed/Zt38koLzE4g" frameborder="0" allowfullscreen></iframe>
+				</div>
+				<div class="col-6">
+				   <h5>One Month HIIT workouts</h5>
+				   <small>
+                   High-intensity interval training (HIIT) has become a popular way to burn fat in the gym, but that's not all this type of cardio training is good for. The essential framework of high-intensity interval training is always the same: Brief, all-out work periods, separated by rest periods that you wish were just a little longer.
+				   </small>
+				</div>
+				<div class="col">
+				<button type="button" class="btn btn-secondary btn-lg">Watch Now</button>
+				</div>
+			</div>
+		</div>
+</section>
+
+
+
+
+<?php 
+ include('footer.php');
+?>
