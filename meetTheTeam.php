b1edbc0ce0a01fd2ac48f58eed826007a86dfa7c
Wisdom-22 <66562629+Wisdom-22@users.noreply.github.com>
2020-06-09 15:29:12 +0100

    This is meet the team page

---
+++ meetTheTeam.php
@@ -0,0 +1,31 @@
+<?php include ('header.php');?> <!--including header-->
+    <h2>The brains behind our company</h2>
+    <section class ="container"> <!--Page contains only one section -->
+        <div class ="meetperson1">
+          <img src="images/meet1.jpg">
+          <p>This is Allen Shaw, he has been with us from day 1 of creating the company. He is a cerified fitness trainer and his speciality is helping our clients build muscles. So if a clients need is to bulid muscles, he is the guy for the job. He will also help with the type of exercise you need to, dieting etc.</p>
+        </div>
+
+        <div class ="meetperson2">
+          <img src="images/meet2.jpg">
+          <p>
+		  This Natalie Banks, she studied Nutrition and physical fitness in Harvard University. She is the key individual to talk to for anything that has to with nutrition. No matter the body goals you want, she can help you with the type of food, quantity food, friuts and vegetables that will help you get your body goals in a safe nutritional way.
+		  </p>
+          
+        </div>
+        <div class ="meetperson3">
+          <img src="images/meet3.jpg">
+          <p>This is Billy Smith, he is our company's physioterapist who graduated from Trinity College. He mustly works with people who are recovering from a very fatal injuries. He has been with us for 3 years and during that time many of our clients he has worked with have gotten good satisfying results.  </p>
+        </div>
+
+        <div class ="meetperson4">
+          <img src="images/meet4.jpg">
+          <p>
+		       Shelly Foster is certified physical trainer that studied physical fitness in Harvard University and graduated top of her class. She has worked with clients who like to bulk up their body. She has developed technique and methods that aid in bulking up her clients in a nutritional and healthy way.
+		      </p>
+        </div>
+
+    </section>
+    <?php include ('footer.php'); ?> <!--including footer-->
+  </body>
+</html>
