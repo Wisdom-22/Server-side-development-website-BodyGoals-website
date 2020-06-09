64fe4c304d612d70a864b1660ea7a6f7ac12ca81
Wisdom-22 <66562629+Wisdom-22@users.noreply.github.com>
2020-06-09 15:45:01 +0100

    Thi is the testimonial manage 1 page

---
+++ testimonial_manage1.php
@@ -0,0 +1,33 @@
+<!--Written by Chukwuemeka Wisdom Arinze-->
+<head>
+	<link rel ="stylesheet" href="bodygoals.css">
+</head>
+<section class = "testimonial">
+		<div class = "container-for-testimonial">
+			 <div class="testimonial-text1">
+				<h2 class="featurette-heading">First featurette heading. <span class="text-muted">It’ll blow your mind.</span></h2>
+				<p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+			</div>
+			
+			<div class="testimonial-img1">
+				<!--<svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"></rect><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>-->
+				<img src = "images/icon.jpg" alt = ""/> 
+			  </div>
+		 </div>
+		 
+		 
+		 <div class = "container-for-testimonial">
+		
+			  <div class="testimonial-img2">
+					<!--<svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"></rect><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>-->
+					<img src = "images/icon.jpg" alt = ""/>
+			 </div>
+			
+			<div class="testimonial-text2">
+				<h2 class="featurette-heading">And lastly, this one. <span class="text-muted">Checkmate.</span></h2>
+					<p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+			</div>
+			
+		</div>
+	</section>	
+	
