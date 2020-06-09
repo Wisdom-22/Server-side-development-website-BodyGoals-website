10fb26f8380d6bb4db4e94238ca3fc6bffe284da
Wisdom-22 <66562629+Wisdom-22@users.noreply.github.com>
2020-06-09 15:30:10 +0100

    This is the nutrition page

---
+++ Nutrition.php
@@ -0,0 +1,86 @@
+<?php 
+ include('header.php');
+?>
+<section class = "trainer"  style= "padding:50px; text-align:center;">
+    <h3>Welcome</h3>
+		<div class="container">
+			<div class="row">
+				<div class="col">
+				   <iframe width ="300" height ="200" src ="https://www.youtube.com/embed/fR3NxCR9z2U" frameborder="0" allowfullscreen></iframe>
+				</div>
+				<div class="col-6">
+				   <h5>Introduction to Metabolism and Nutrition</h5>
+				   <small>
+                   Introduction to nutrition. Nutrition is defined as the processes by which an animal or plant takes in and utilises food substances. Essential nutrients include protein, carbohydrate, fat, vitamins, minerals and electrolytes. Normally, 85% of daily energy use is from fat and carbohydrates and 15% from protein.
+				   </small>
+				</div>
+				<div class="col">
+				<button type="button" class="btn btn-secondary btn-lg">Watch Now</button>
+				</div>
+			</div>
+		</div>
+</section>
+
+<section class = "trainer"  style= "padding:50px; text-align:center;">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+				   <iframe width ="300" height ="200" src ="https://www.youtube.com/embed/3XMh6f0xB7A" frameborder="0" allowfullscreen></iframe>
+				</div>
+				<div class="col-6">
+				   <h5>Key to Overall Athlete Performance</h5>
+				   <small>
+                   Nutrition and athletic performance Nutrition can help enhance athletic performance. An active lifestyle and exercise routine, along with eating well, is the best way to stay healthy. Eating a good diet can help provide the energy you need to finish a race, or just enjoy a casual sport or activity.
+				   </small>
+				</div>
+				<div class="col">
+				<button type="button" class="btn btn-secondary btn-lg">Watch Now</button>
+				</div>
+			</div>
+		</div>
+</section>
+
+<section class = "trainer"  style= "padding:50px; text-align:center;">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+				   <iframe width ="300" height ="200" src ="https://www.youtube.com/embed/7vGzibcZD5o" frameborder="0" allowfullscreen></iframe>
+				</div>
+				<div class="col-6">
+				   <h5>Nutrition for fat loss</h5>
+				   <small>
+                   The plan is designed to help you lose weight at a safe rate of 0.5kg to 1kg (1lb to 2lb) each week by sticking to a daily calorie allowance. For most men, this means sticking to a calorie limit of no more than 1,900kcal a day, and 1,400kcal for most women. If you find it hard sticking to the calorie limit.
+				   </small>
+				</div>
+				<div class="col">
+				<button type="button" class="btn btn-secondary btn-lg">Watch Now</button>
+				</div>
+			</div>
+		</div>
+</section>
+
+<section class = "trainer"  style= "padding:50px; text-align:center;">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+				   <iframe width ="300" height ="200" src ="https://www.youtube.com/embed/w6bGb4yt76s" frameborder="0" allowfullscreen></iframe>
+				</div>
+				<div class="col-6">
+				   <h5>Nutrition for mass gainer</h5>
+				   <small>
+                   Whether you’re looking to bulk up or get lean, clean up your diet with this meal plan. These seven foods give you the protein and carb punch you need to gain mass. These surprising foods can help you bulk up in no time. Carbohydrates are essential to support your fitness goals.
+				   </small>
+				</div>
+				<div class="col">
+				<button type="button" class="btn btn-secondary btn-lg">Watch Now</button>
+				</div>
+			</div>
+		</div>
+</section>
+
+
+
+
+<?php 
+ include('footer.php');
+?>
