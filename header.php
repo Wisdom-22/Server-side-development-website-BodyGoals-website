1dac66df0adfaeaa999ea88e8395748002916e4b
Wisdom-22 <66562629+Wisdom-22@users.noreply.github.com>
2020-06-09 15:25:07 +0100

    This is the header page

---
+++ header.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang = "en">
+	<head>
+		<!-- head section --> 
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<title>BodyGoals</title>
+		<link rel ="icon" href ="images/icon">
+		<link rel ="stylesheet" href="bodygoals.css">
+		<link rel ="stylesheet" href="meetTeam.css">
+		<link rel ="stylesheet" href="bookclasses.css">
+		<link rel="canonical" href="https://getbootstrap.com/docs/4.4/examples/carousel/">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+		<!-- Bootstrap core CSS -->
+		<link href="/docs/4.4/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+		<!--boostrap javaScript -->
+		<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+		<script src="https://kit.fontawesome.com/28ced351d4.js" crossorigin="anonymous"></script>
+		
+			<!-- Favicons -->
+		<link rel="apple-touch-icon" href="/docs/4.4/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
+		<link rel="icon" href="/docs/4.4/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
+		<link rel="icon" href="/docs/4.4/assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
+		<link rel="manifest" href="/docs/4.4/assets/img/favicons/manifest.json">
+		<link rel="mask-icon" href="/docs/4.4/assets/img/favicons/safari-pinned-tab.svg" color="#563d7c">
+		<link rel="icon" href="/docs/4.4/assets/img/favicons/favicon.ico">
+		<meta name="msapplication-config" content="/docs/4.4/assets/img/favicons/browserconfig.xml">
+		<meta name="theme-color" content="#563d7c">
+	</head>
+	<body id = "page">
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark" id="headernav">
+      <a href = "index.php"> 
+			<i class="icon fas fa-dumbbell fa-5x"></i>
+	  </a>
+		
+	  <a class="navbar-brand" href="index.php"> <strong>BODYGOALS</strong></a>
+	  
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded=	"false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+      </button>
+          <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+				  <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+					  <div class="dropdown">
+						  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							Users
+						  </button>
+						  <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+							<button class="dropdown-item" type="button">Admin</button>
+							<button class="dropdown-item" type="button">Member</button>
+							<button class="dropdown-item" type="button">Public</button>
+						  </div>
+					 </div>
+				 
+					<li class="nav-item">
+						<a class="nav-link" href="bookclasses.php">Book classes 2020</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="testimonial_add.php">Testimonials</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="meetTheTeam.php">Meet the Team </a>
+					</li>
+			
+		      </ul>
+			  
+			  <form class="form-inline my-2 my-lg-0" action = "login.php">
+					<button class="btn btn-outline-secondary my-2 my-sm-0" type="submit" style="margin: 20px 10px;">Login</button>
+			  </form>
+			  <form class="form-inline my-2 my-lg-0" action = "registration.php">
+				<button class="btn btn-outline-secondary my-2 my-sm-0" type="submit" style="margin: 20px 10px;">Register</button>
+			  </form>
+			  <form class="form-inline my-2 my-lg-0" action="Logout.php ">
+       			<button class="btn btn-outline-secondary my-2 my-sm-0" type="submit" style="margin: 20px 10px;">Logout</button>
+      		</form>
+    </div>
+  </nav>
+  
+	</body>
+</html>
+
