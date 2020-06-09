6659f281208b5686f46a4673b703bc86e4d41bf0
Wisdom-22 <66562629+Wisdom-22@users.noreply.github.com>
2020-06-09 15:22:11 +0100

    This is the events page

---
+++ events.php
@@ -0,0 +1,106 @@
+<!-- written by muiz Johnson -->
+<?php 
+ include('header.php');
+?>
+
+<section style= "padding:50px; text-align:center;" > 
+    <div class="container">
+    <div class="row">
+        <div class="col">
+            <h5><strong>13</strong></h5>
+            <h6>May 2020</h6>
+            <i class="fas fa-clock"></i>
+            <h6>May 1 - May 10</h6>
+        </div>
+        <div class="col">
+           <img style = "max-width:180px;" src = "images/bike.jpg" alt = ""/>
+        </div>
+        <div class="col-6">
+           <h5>Bike Competition</h5>
+           <small>This event features a competetion in which all the professional bikers of every gym in dublin comes together to take place in this competition.
+           From motorcycle shows and events, to social meets, race meets and track days, check out what's going on in 2020. More events are being added all the time, so keep coming back to see whats on, where and when
+           </small>
+        </div>
+        <div class="col">
+        <i class="fas fa-search-location"></i><p></p>
+        <small>40 Avenue Road</small>
+        <small>Tom Bellew Avenue</small>
+        <small>Dundrum , Ireland.</small>
+        </div>
+    </div> 
+</div> 
+</section>
+
+<section>
+    <div class="container">
+    <div class="row">
+        <div class="col">
+            <h5><strong>17</strong></h5>
+            <h6>June 2020</h6>
+            <i class="fas fa-clock"></i>
+            <h6>June 17 - June 20</h6>
+        </div>
+        <div class="col">
+           <img style = "max-width:180px;" src = "images/event1.jpg" alt = ""/>
+        </div>
+        <div class="col-6">
+           <h5>Dublin Gymastic competetion 2020</h5>
+           <small>This event features a competetion in which all the professional gym members of every gym in dublin comes together to take place in this competition.
+           Men's and women's gymnastics include two common events, the floor exercise and the vault. Otherwise, the sport varies for male and female competitors. Men compete in six events, and women compete in four.
+           </small>
+        </div>
+        <div class="col">
+        <i class="fas fa-search-location"></i><p></p>
+        <small>1 Henry Street</small>
+        <small>O'Conell Street</small>
+        <small>Dublin, Ireland.</small>
+        </div>
+    </div>
+</div>
+</section>
+
+<section>
+    <div class="container">
+    <div class="row">
+        <div class="col">
+            <h5><strong>21</strong></h5>
+            <h6>July 2020</h6>
+            <i class="fas fa-clock"></i>
+            <h6>July 21 - July 21</h6>
+        </div>
+        <div class="col">
+           <img style = "max-width:180px;" src = "images/arnold1.jpg" alt = ""/>
+        </div>
+        <div class="col-6">
+           <h5>Arnold Pro Show</h5>
+           <small>The Arnold Classic Pro Show at the Arnold Sports Festival began as the Australian Pro Grand prix and is the third-longest running Pro Show in the world.
+            This a unique oportunity to see the top  Olympians and Pro Bodybuilders compete on stage in Melbourne. It’s the biggest event on the calendar for Bodybuilding in Australia!
+           </small>
+        </div>
+        <div class="col">
+        <i class="fas fa-search-location"></i><p></p>
+        <small>12 Melbourne Convention</small>
+        <small>Exibition center</small>
+        <small>Melbourne, Australia.</small>
+        </div>
+    </div>
+</div>
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include('footer.php');?>
