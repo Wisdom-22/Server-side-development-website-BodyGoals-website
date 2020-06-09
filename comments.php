396c9a50ed85c630e0317ce87f6a132743484ccb
Chukwuemeka Wisdom Arinze <chukwuemekawisdom.arinze@student.griffith.ie>
2020-06-09 15:06:18 +0100

    This is comments page

---
+++ comments.php
@@ -0,0 +1,52 @@
+<!--Written by Chukwuemeka Wisdom Arinze-->
+<body id ="page">
+<?php include('header.php');?>
+			  
+			<section class = "registration">  
+				
+				<form class = "form" method = "POST" action = "commentsValidation.php">
+					<h4>Comment</h4>
+						
+						 <div class="registration-textfield">
+						   <label for="firstname" class = "labels-for-textbox">First name</label>
+						   <input type="text" class="form-control" name="firstname" placeholder="" value="" required=""></br>
+							<div class="invalid-feedback">
+							  Valid first name is required.
+							</div>
+						  </div>
+						
+						<div class="registration-textfield">
+						   <label for="firstname" class = "labels-for-textbox">Class name</label>
+						   <input type="text" class="form-control" name="classname" placeholder="" value="" required=""></br>
+							<div class="invalid-feedback">
+							  Valid class name is required.
+							</div>
+						</div>
+						
+						<div class="registration-textfield">
+							<label for="date" class = "labels-for-textbox">Date</label>
+							<input type = "date" class="form-control" name = "date" required/><br>
+							<div class="invalid-feedback">
+								Valid Date is required.
+							</div>
+						</div>
+					  
+					 
+					  
+					  <div class = "registration-textfield">
+							<label class = "labels-for-textbox" for="email">Comment </label>
+								<textarea id = "" rols = "4" cols = "70" name = "comment">
+											 
+								</textarea>
+								<div class="invalid-feedback">
+									Please enter a valid Message here to be in contact with us.
+								</div>
+					 </div>
+			  
+					<button type = "submit" class = "submitbutton">Send Comment</button>
+				</form>
+			 </section> 
+<?php include('footer.php');?>
+
+	</body>
+</html><!--https://wireframe.cc/sPvyz8-->
