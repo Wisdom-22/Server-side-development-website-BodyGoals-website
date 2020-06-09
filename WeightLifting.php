629e607f1438bd18c2af9efbe55df97035d02a70
Wisdom-22 <66562629+Wisdom-22@users.noreply.github.com>
2020-06-09 15:57:50 +0100

    This is the weight lifting page

---
+++ WeightLifting.php
@@ -0,0 +1,86 @@
+<?php 
+ include('header.php');
+?>
+<section class = "trainer"  style= "padding:50px; text-align:center;">
+    <h3>Welcome</h3>
+		<div class="container">
+			<div class="row">
+				<div class="col">
+				   <iframe width ="300" height ="200" src ="https://www.youtube.com/embed/B9moGJHmJyg" frameborder="0" allowfullscreen></iframe>
+				</div>
+				<div class="col-6">
+				   <h5>History of WeightLifting</h5>
+				   <small>
+                   History A long history. As a means to measure strength and power, weightlifting was practised both by ancient Egyptian and Greek... Power struggle. At the beginning of the century, Austria, Germany and France were the most successful nations. 
+				   </small>
+				</div>
+				<div class="col">
+				<button type="button" class="btn btn-secondary btn-lg">Watch Now</button>
+				</div>
+			</div>
+		</div>
+</section>
+
+<section class = "trainer"  style= "padding:50px; text-align:center;">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+				   <iframe width ="300" height ="200" src ="https://www.youtube.com/embed/vYCIqtJ1UiE" frameborder="0" allowfullscreen></iframe>
+				</div>
+				<div class="col-6">
+				   <h5>Compound Excercises</h5>
+				   <small>
+                   Compound exercises are exercises that work multiple muscle groups at the same time. For example, a squat is a compound exercise that works the quadriceps, glutes, and calves. You can also do compound exercises that combine two exercises into one move to target even more muscles (for example, a lunge with a bicep curl).
+				   </small>
+				</div>
+				<div class="col">
+				<button type="button" class="btn btn-secondary btn-lg">Watch Now</button>
+				</div>
+			</div>
+		</div>
+</section>
+
+<section class = "trainer"  style= "padding:50px; text-align:center;">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+				   <iframe width ="300" height ="200" src ="https://www.youtube.com/embed/Za4182MoMPs" frameborder="0" allowfullscreen></iframe>
+				</div>
+				<div class="col-6">
+				   <h5>WeightLifting for Olympics</h5>
+				   <small>
+                   Olympic-style weightlifting, or Olympic weightlifting, often simply referred to as weightlifting, is an athletic discipline in the modern Olympic program in which the athlete attempts a maximum-weight single lift of a barbell loaded with weight plates. The two competition lifts in order are the snatch and the clean and jerk.
+				   </small>
+				</div>
+				<div class="col">
+				<button type="button" class="btn btn-secondary btn-lg">Watch Now</button>
+				</div>
+			</div>
+		</div>
+</section>
+
+<section class = "trainer"  style= "padding:50px; text-align:center;">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+				   <iframe width ="300" height ="200" src ="https://www.youtube.com/embed/uYvUx56WFeM" frameborder="0" allowfullscreen></iframe>
+				</div>
+				<div class="col-6">
+				   <h5>General WeightLifting training</h5>
+				   <small>
+                   The general phase of training for Weightlifters is an often overlooked but valuable part of the training process. This time creates the foundation of muscle size, strength, work capacity and movement qualities that big snatch and clean & jerks at meets are built upon. In this video, Max Aita discusses how he approaches this phase of training.
+				   </small>
+				</div>
+				<div class="col">
+				<button type="button" class="btn btn-secondary btn-lg">Watch Now</button>
+				</div>
+			</div>
+		</div>
+</section>
+
+
+
+
+<?php 
+ include('footer.php');
+?>
