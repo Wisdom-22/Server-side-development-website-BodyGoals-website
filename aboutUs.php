57615eae95ea818cb49a98b7f905cdea43a35c90
Chukwuemeka Wisdom Arinze <chukwuemekawisdom.arinze@student.griffith.ie>
2020-06-09 14:48:19 +0100

    This is the about us page

---
+++ aboutUs.php
@@ -0,0 +1,36 @@
+<!--written by muiz johnson-->
+<?php include('header.php'); ?>
+
+<section style ="text-align:center; padding:40px;">
+    <h2>About Us</h2>
+    <em class ="lead">Our Motto: Dream it , Achieve it.</em>
+<div class="container">
+  <div class="row">
+    <div class="col">
+       <h4> Our Mission </h4>
+       <small>Our Mission is to Empower You Total Fitness is a team of committed and caring trainers who are striving to empower individuals to a healthier lifestyle. We will work within our Code of Values to ensure that everyone we train will move closer to becoming the person they want to become and achieving the goals they want to achieve.Our client base in rapidly increasing at currently 1 million globally join us and become one of us.</small>
+    </div>
+    <div class="col">
+     <h4> Our Essence </h4>
+     <small>Bodygoals is a fitness studio based on the fact that a workout should be an inclusive, challenging, and empower you from the inside out. Known for it's 45 minute HIIT training and strength offerings.We actively coach you to use your body the way nature intended it to be used and focus on a functional approach to fitness. We want our members not just looking good, but to have strong, healthy and supple bodies that support us in all aspects of life.</small>
+    </div>
+</div>
+
+<div class="container">
+  <div class="row">
+    <div class="col">
+       <h4> Our Promise </h4>
+       <small>At bodygoals , we deliver optimistic and diverse bodybuilding, experiences , and points of view to our determined clients all over the world and guarantee results in less than one month.The January resolve to get in shape in 2021 can stay with you through the year. Finding time, energy and motivation to keep your fitness promise means commitment. 
+        A healthy lifestyle commitment is not something you do for anyone but you.</small>
+    </div>
+    <div class="col">
+     <h4> Our Vibe </h4>
+     <small>At bodygoals, we make magic through bodybuilding , we dream it , and we go out there to achieve it and then do it together everyday reshaping our body to the best it can get.We offer more than just a gym membership. Our members enjoy a lifestyle of healthy living, accompanied by first class advice and training programmes tailor-made by our fitness team to make sure each individual’s goals are met.</small>
+    </div>
+</div>
+
+</section>
+
+
+
+<?php include('footer.php'); ?>
