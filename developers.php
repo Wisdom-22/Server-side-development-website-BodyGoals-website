08becb4f8a60ff19d265077d35054067935521d6
Wisdom-22 <66562629+Wisdom-22@users.noreply.github.com>
2020-06-09 15:21:29 +0100

    This is the developers page

---
+++ developers.php
@@ -0,0 +1,38 @@
+ <!--written by muiz johnson-->
+ <body id ="page">
+  <?php include('header.php');?>
+	
+  <section style= "padding:50px; text-align:center;">
+		<div class="row">
+			<div class="col-8">
+					<p>
+					My name is Muiz Johnson born and raised in lagos , Nigeria. I am currently in my second year in griffith college aspring to bag 
+					my bachelors degree in computer science and masters in future. I decided to study computer science because there is a lot of interesting 
+					facts i would love to know like maching learning , artificial intelligence and so on. 
+					</p>
+			</div>
+			<div class="col-4">
+				<img style = "max-width:200px;" src = "images/developer1" alt = ""/> 
+			</div>
+		</div>
+	</section>
+	
+	<section style= "padding:50px; text-align:center;">
+		<div class="row">
+			<div class="col-8">
+					<p>
+						My name is Chukwuemeka Wisdom Arinze and i am from Nigeria. I am a student in my second year, currently studying Computer science in Griffith College. 
+					The reason i chose computer science as my field of study is because, the world is forever evolving and the need for people who study in this field is enormous.
+					</p>
+			</div>
+				<div class="col-4">
+				<img style = "max-width:200px;" src = "images/developer2" alt = ""/> 
+				</div>
+		</div>
+	</section>
+
+   
+ <?php include('footer.php');?>
+
+</body>
+</html><!--https://wireframe.cc/sPvyz8-->
