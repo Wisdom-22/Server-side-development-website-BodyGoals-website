e6c624370db6603517110007b3f7d2f32807281b
Chukwuemeka Wisdom Arinze <chukwuemekawisdom.arinze@student.griffith.ie>
2020-06-09 14:56:18 +0100

    This is body building page

---
+++ BodyBuilding.php
@@ -0,0 +1,88 @@
+
+<?php 
+ include('header.php');
+?>
+<section class = "trainer"  style= "padding:50px; text-align:center;">
+    <h3>Welcome</h3>
+		<div class="container">
+			<div class="row">
+				<div class="col">
+				   <iframe width ="300" height ="200" src ="https://www.youtube.com/embed/WZMN0CoSRwE" frameborder="0" allowfullscreen></iframe>
+				</div>
+				<div class="col-6">
+				   <h5>Introduction to Body Building</h5>
+				   <small>
+                   Start here with beginner yoga sequences, tutorials on foundational body building, and answers to your questions on all the basics. Build strength and confidence to shape your body better.
+                   Even if you have chronic fatigue, fibromyalgia, or autoimmune diseases like Lyme or rheumatoid arthritis and have to spend part (or most) of the day.We can still build you up.
+				   </small>
+				</div>
+				<div class="col">
+				<button type="button" class="btn btn-secondary btn-lg">Watch Now</button>
+				</div>
+			</div>
+		</div>
+</section>
+
+<section class = "trainer"  style= "padding:50px; text-align:center;">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+				   <iframe width ="300" height ="200" src ="https://www.youtube.com/embed/ZxJlZxZ2KFw" frameborder="0" allowfullscreen></iframe>
+				</div>
+				<div class="col-6">
+				   <h5>Muscle Building 101</h5>
+				   <small>
+                   It is a complete list of most advanced muscle building exercises  along with some intermediate positions modified to level up your home muscles and muscle memory  for improved strength, mental focus, flexibility and better health and body. It’s time to get going and challenge everyone by performing the hardest exercises with ease.
+				   </small>
+				</div>
+				<div class="col">
+				<button type="button" class="btn btn-secondary btn-lg">Watch Now</button>
+				</div>
+			</div>
+		</div>
+</section>
+
+<section class = "trainer"  style= "padding:50px; text-align:center;">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+				   <iframe width ="300" height ="200" src ="https://www.youtube.com/embed/Umu4cxTPaC8" frameborder="0" allowfullscreen></iframe>
+				</div>
+				<div class="col-6">
+				   <h5>Best Advanced Routines</h5>
+				   <small>
+                   Crushing your goals for the new year and beyond has never been easier with Bodygoals single fitness solution: BodyFit. Nutrition Recipes. Valentine's Day Protein Lasagna. A recipe that will spark the flame and keep you both on track for gains. Find a workout plan. that works for you. 
+				   </small>
+				</div>
+				<div class="col">
+				<button type="button" class="btn btn-secondary btn-lg">Watch Now</button>
+				</div>
+			</div>
+		</div>
+</section>
+
+<section class = "trainer"  style= "padding:50px; text-align:center;">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+				   <iframe width ="300" height ="200" src ="https://www.youtube.com/embed/o9zCgPtsups" frameborder="0" allowfullscreen></iframe>
+				</div>
+				<div class="col-6">
+				   <h5>Train for Mass</h5>
+				   <small>
+                   Staying lean isn't as complicated as it may seem. To keep body fat low and showcase your hard-earned muscle, cardio and conditioning are a crucial part of your workout schedule. Most people know this, so it's not really life-changing news. 
+				   </small>
+				</div>
+				<div class="col">
+				<button type="button" class="btn btn-secondary btn-lg">Watch Now</button>
+				</div>
+			</div>
+		</div>
+</section>
+
+
+
+
+<?php 
+ include('footer.php');
+?>
